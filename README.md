0137990dd84f219b460a7c6ab2b03bc0085cbc1c
Kristof Mattei <github@kristofmattei.be>
2015-03-01 19:22:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Supporting project for: http://stackoverflow.com/questions/28796757/dynamodb-with-conditionexpression
---
+++ dynamo-db-fights/app.js
@@ -0,0 +1,39 @@
+﻿var vogels = require("vogels");
+var Joi = require("Joi");
+
+vogels.AWS.config.update({ region: "eu-west-1" });
+
+var foobar = vogels.define("foo-bar", {
+    tableName: "foo-bar",
+    hashKey: "foo",
+    rangeKey: "bar",
+    schema: {
+        "foo": Joi.string(),
+        "bar": Joi.string(),
+        "name": Joi.string()
+    }
+});
+
+var itemToInsert = {
+    foo: "foo",
+    bar: "bar2",
+    name: "TEST"
+};
+
+var params = {
+    ConditionExpression: "foo <> :foo AND name <> :name",
+    ExpressionAttributeValues: {
+        ":foo": itemToInsert.foo,
+        ":name": itemToInsert.name
+    }
+};
+
+foobar
+    .create(itemToInsert, params, function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(data);
+        }
+    });
